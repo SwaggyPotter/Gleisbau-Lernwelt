7e6b64964e66131c8e37a3bc87caa3f5f2d1864e
Tim <89969636+SwaggyPotter@users.noreply.github.com>
2026-01-13 20:09:46 +0100

    First Push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gleisbau.lernwelt',
+  appName: 'Gleisbau Lernwelt',
+  webDir: 'www'
+};
+
+export default config;
